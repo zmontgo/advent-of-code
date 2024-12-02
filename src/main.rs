2ac01772502669f49215a94c13f12196027ea49d
Zachary Montgomery <zachmontgo@iodinedev.com>
2024-12-02 16:50:37 -0600

    Naive day selection (guys it's production-ready now)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
-// mod one;
+use std::env;
+
+mod one;
 mod two;
 
 use std::fs;
 
 fn main() {
-    // one::day_one();
-    two::day_two();
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+
+    match query.as_str() {
+        "one" => one::day_one(),
+        "two" => two::day_two(),
+        _ => println!("Invalid query."),
+    };
 }
 
 fn read_file(path: String) -> String {
-    fs::read_to_string(path).expect("Something went wrong reading the file")
+    match fs::read_to_string(path) {
+        Ok(contents) => contents,
+        Err(_) => panic!("Error reading file. Make sure to add your puzzle input in a file called `input.txt` in the folder of the day you're choosing."),
+    }
 }
